7da780dfe579247344f2284fbb28899c188e9ed1
Matthew Broatch <mnbroatch@gmail.com>
2022-12-06 23:31:25 -0800

    fix clue problem

--- get-letter-states.js
+++ get-letter-states.js
@@ -3,16 +3,13 @@ export default function getLetterStates (answer, guess) {
     if (answer[i] === letter) {
       return [...acc, 'correct']
     } else if (answer.includes(letter)) {
-      // Only color first n instances,
-      // where n === number of unguessed instances in answer
-      const instancesSoFarCount = guess.split('').slice(0, i)
+      const instancesInAnswerCount = answer.split('')
         .filter(l => l === letter).length
-      const instancesInWordNotGuessedCount = answer.split('')
-        .filter((l, j) => l === letter && guess[j] !== letter)
-        .length
+      const instancesSoFarInGuessCount = guess.split('').slice(0, i)
+        .filter(l => l === letter).length
       return [
         ...acc,
-        instancesSoFarCount < instancesInWordNotGuessedCount
+        instancesSoFarInGuessCount < instancesInAnswerCount
           ? 'present'
           : 'absent'
       ]
